009c71543dbb209a309461f6f78945425262f0e5
Vladimir Skrypnikov <sevgrad@list.ru>
2024-01-29 08:16:35 +0300

    Third homework complete

--- HomeWork_3/PhoneBook_v3/src/main/kotlin/Command.kt
+++ HomeWork_3/PhoneBook_v3/src/main/kotlin/Command.kt
@@ -1,92 +1,93 @@
 sealed interface Command {
     fun isValid(): Boolean
+}
 
-    class Find(
-        private val email: String? = null,
-        private val phone: String? = null,
-        private val personsList: MutableList<Person>
-    ): Command {
-        override fun isValid(): Boolean = true
+class Find(
+    private val email: String? = null,
+    private val phone: String? = null,
+    private val personsList: MutableList<Person>
+): Command {
+    override fun isValid(): Boolean = true
 
-        fun findPersons(): List<Person>? {
-            if (email != null) return personsList.filter { it.emails.contains(email) }
-            if (phone != null) return personsList.filter { it.phones.contains(phone) }
-            return null
-        }
+    fun findPersons(): List<Person>? {
+        if (email != null) return personsList.filter { it.emails.contains(email) }
+        if (phone != null) return personsList.filter { it.phones.contains(phone) }
+        return null
     }
+}
 
-    class AddEmail(
-        private val name: String,
-        private val email: String,
-        private var personsList: MutableList<Person>
-    ): Command {
-        override fun isValid(): Boolean =
-            email.matches(Regex("""[a-z0-9]+@[a-z]+\.[a-z]{2,3}"""))
+class AddEmail(
+    private val name: String,
+    private val email: String,
+    private var personsList: MutableList<Person>
+): Command {
+    override fun isValid(): Boolean =
+        email.matches(Regex("""[a-z0-9]+@[a-z]+\.[a-z]{2,3}"""))
 
-        fun getPerson() {
-            if (isValid()) {
-                val person: Person? = personsList.find { it.name == this.name }
-                if (person == null) {
-                    personsList.add(Person(name = this.name, email = email))
-                    println("Person was created")
-                } else {
-                    person.addEmail(this.email)
-                }
+    fun getPerson() {
+        if (isValid()) {
+            val person: Person? = personsList.find { it.name == this.name }
+            if (person == null) {
+                personsList.add(Person(name = this.name, email = email))
+                println("Person was created")
             } else {
-                println("Wrong email format")
-                Help.printHelp()
+                person.addEmail(this.email)
             }
+        } else {
+            println("Wrong email format")
+            Help.printHelp()
         }
     }
+}
 
-    class AddPhone(
-        private val name: String,
-        private val phone: String,
-        private var personsList: MutableList<Person>
-    ): Command {
-        override fun isValid(): Boolean =
-            phone.matches(Regex("""(\+7|8)[\s(]*\d{3}[)\s]*\d{3}[\s-]?\d{2}[\s-]?\d{2}"""))
+class AddPhone(
+    private val name: String,
+    private val phone: String,
+    private var personsList: MutableList<Person>
+): Command {
+    override fun isValid(): Boolean =
+        phone.matches(Regex("""(\+7|8)[\s(]*\d{3}[)\s]*\d{3}[\s-]?\d{2}[\s-]?\d{2}"""))
 
-        fun getPerson() {
-            if (isValid()) {
-                val person: Person? = personsList.find { it.name == this.name }
-                if (person == null) {
-                    personsList.add(Person(name = this.name, phone = this.phone))
-                    println("Person was created")
-                } else {
-                    person.addPhone(this.phone)
-                }
+    fun getPerson() {
+        if (isValid()) {
+            val person: Person? = personsList.find { it.name == this.name }
+            if (person == null) {
+                personsList.add(Person(name = this.name, phone = this.phone))
+                println("Person was created")
             } else {
-                println("Wrong phone format")
-                Help.printHelp()
+                person.addPhone(this.phone)
             }
+        } else {
+            println("Wrong phone format")
+            Help.printHelp()
         }
     }
+}
 
-    class Show(
-        private val name: String,
-        private val personsList: MutableList<Person>
-    ): Command {
-        override fun isValid(): Boolean = personsList.isNotEmpty()
+class Show(
+    private val name: String,
+    private val personsList: MutableList<Person>
+): Command {
+    override fun isValid(): Boolean = personsList.isNotEmpty()
 
-        fun printPerson() {
-            if (isValid()) {
-                val person: Person? = personsList.find { it.name == this.name }
-                if (person != null) println(person) else println("No such person")
-            } else println("Not initialized")
-        }
+    fun printPerson() {
+        if (isValid()) {
+            val person: Person? = personsList.find { it.name == this.name }
+            if (person != null) println(person) else println("No such person")
+        } else println("Not initialized")
     }
+}
 
-    data object Exit : Command {
-        override fun isValid(): Boolean = true
-    }
+data object Exit : Command {
+    override fun isValid(): Boolean = true
+}
 
-    data object Help : Command {
-        override fun isValid(): Boolean = true
+data object Help : Command {
+    override fun isValid(): Boolean = true
 
-        fun printHelp() {
-            println(
-                """
+    fun printHelp() {
+        println(
+            """
                 ======================================================================================
                 exit - Выход
                 help - Помощь
@@ -101,7 +102,6 @@ sealed interface Command {
                 find email <Адрес электронной почты> - Вывести список контактов с таким адресом
                 ======================================================================================
                 """.trimIndent()
-            )
-        }
+        )
     }
 }
--- HomeWork_3/PhoneBook_v3/src/main/kotlin/Person.kt
+++ HomeWork_3/PhoneBook_v3/src/main/kotlin/Person.kt
@@ -1,10 +1,10 @@
 class Person(
-    private val name: String,
+    val name: String,
     phone: String? = null,
     email: String? = null
 ) {
-    private var phones: MutableList<String> = mutableListOf()
-    private var emails: MutableList<String> = mutableListOf()
+    var phones: MutableList<String> = mutableListOf()
+    var emails: MutableList<String> = mutableListOf()
 
     init {
         if (phone != null) phones.add(phone)
